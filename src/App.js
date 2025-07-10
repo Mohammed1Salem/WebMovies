1cb09362176dbc8dff9d7078a181450fd2e2525e
Mastie1 <mkm3361@gmail.com>
2025-07-10 08:45:36 +0300

    small changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
     <div className="App">
       <div className="container">
         <Header />
-        <button> click me yes</button>
         <MoviesGrid />
       </div>
       <Footer />
