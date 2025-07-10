98f3a320b972f7791abbfddbf45aa6e08b53b4bf
Mastie1 <mkm3361@gmail.com>
2025-07-11 00:22:57 +0300

    Routers added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import "./styles.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MoviesGrid from "./components/MoviesGrid";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header />
-        <MoviesGrid />
+
+        <Router>
+          <nav></nav>
+          <Routes>
+            <Route path="/" element={<MoviesGrid />}></Route>
+          </Routes>
+        </Router>
       </div>
       <Footer />
     </div>
